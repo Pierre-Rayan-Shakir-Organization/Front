0b5ca3ffaa33ed76ddd0dfea952d46ece2e92b88
Shakir <shakir.laabany@parisnanterre.fr>
2024-12-18 11:43:43 +0100

    ajout d'un domaine dans nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn-images.dzcdn.net', 'e-cdns-images.dzcdn.net', 'via.placeholder.com'], // Ajoutez ici le domaine problématique
+    domains: ['cdn-images.dzcdn.net', 'e-cdns-images.dzcdn.net', 'via.placeholder.com', 'example.com'], // Ajoutez ici le domaine problématique
   },
 };
 
